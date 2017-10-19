b986cb94d3143a9cc4bc0616bf17be8ddeb457ea
prem <premkumar.bhaskal@nokia.com>
2017-10-19 21:04:31 +0530

    ok now. some improvement in the group info UI.

--- app/src/main/java/com/example/bhaskal/myapplication/ChatInfo.java
+++ app/src/main/java/com/example/bhaskal/myapplication/ChatInfo.java
@@ -3,11 +3,11 @@ package com.example.bhaskal.myapplication;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class ChatDetails extends Activity {
+public class ChatInfo extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat_details_list);
+        setContentView(R.layout.activity_gpinfo_details_list);
     }
 }
